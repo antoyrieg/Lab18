3597965219daae58d2f9921b704ebd8f7386a588
tinwindow <erin.ura@gmail.com>
2017-08-10 22:11:43 -0400

    partialcssapi

--- JS/sentimentcontroller.js
+++ JS/sentimentcontroller.js
@@ -1,22 +1,20 @@
+<<<<<<< Updated upstream
+var app = angular.module ("myModule");
+app.controller("sentimentController", function($scope, pService) {
+
+});
+=======
+(function() {
 var app = angular.module("myModule");
 
-app.controller("sentimentController", function($scope, sentimentService, $http) {
+app.controller("sentimentController", function($scope, sentimentService) {
+    $scope.setSentiment = function(wordstoeval) {
+       $scope.response = sentimentService.sentimentTranslate(wordstoeval);
+       console.log($scope.response);
+    };
 
-  console.log("I'm running.")
-  $http({
-    method: 'POST',
-    url: "https://community-sentiment.p.mashape.com/text/",
-    headers: {
-      "X-Mashape-Key": '5i0OBBkMW6mshw90Jkql7723n2yUp1n96uHjsnScE4aYbm3gKt',
-      'Content-Type': 'application/x-www-form-urlencoded',
-      "Accept": "application/json"
-    },
-    data: "txt=" + encodeURIComponent("happy")
-  }).then(function(response) {
-    console.log(response.data);
-  }).catch(function(response) {
-    console.log("FAIL", response);
-  })
-})
 
+  });
+});
 //var promise = $http.get('https://community-sentiment.p.mashape.com/text');
+>>>>>>> Stashed changes
